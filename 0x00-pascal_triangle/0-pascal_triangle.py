764b3bb0e663055884b4f974007ce6fcb8bb2c27
Mohamed Ahmed <ma2286572@gmail.com>
2024-06-30 11:47:14 +0000

    Write a shorter code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
 """
-Module
-Pascal Triangle Interview Challenge
+Pascal's triangle implementation
 """
 
 
 def pascal_triangle(n):
     """
-    This function returns a list of lists of numbers
-    representing pascal triangle
+    Return pascal's triangle of n rows
     """
-    if n <= 0:
+    if (n <= 0):
         return []
-
-    pascal_triangle = [0] * n
-
-    for i in range(n):
-        # define a row and fill first and last idx with 1
-        new_row = [0] * (i+1)
-        new_row[0] = 1
-        new_row[len(new_row) - 1] = 1
-
+    triangle = [[1]]
+    for i in range(1, n):
+        new_row = triangle[i - 1] + [1]
         for j in range(1, i):
-            if j > 0 and j < len(new_row):
-                a = pascal_triangle[i - 1][j]
-                b = pascal_triangle[i - 1][j - 1]
-                new_row[j] = a + b
-
-        pascal_triangle[i] = new_row
-
-    return pascal_triangle
+            new_row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(new_row)
+    return triangle
