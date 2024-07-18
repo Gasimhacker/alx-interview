280d4fc18ad072652e0147b5d6c29013321793f5
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-18 18:02:49 +0000

    Check if the error is with the regex pattern

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,14 +5,21 @@ import re
 import signal
 
 
-ip = r'(\d{3}\.){3}\d{3}'
-http_request = '"GET /projects/260 HTTP/1.1"'
+ip = r'(\d{1-3}\.){3}\d{1-3}'
+http_request = r'\"GET /projects/260 HTTP/1.1\"'
 status_code_r = r' 200 | 301 | 400 | 401 | 403 | 404 | 405 | 500 '
 file_size_r = r'\d*$'
-date = r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d*\]'
+date = r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\]'
 pattern = f'{ip} - {date} {http_request}{status_code_r}{file_size_r}'
 
-
+fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                 '403': 0, '404': 0, '405': 0, '500': 0
                 }
@@ -37,7 +44,7 @@ signal.signal(signal.SIGINT, handler)
 
 for line in sys.stdin:
     count += 1
-    m = re.search(pattern, line)
+    m = re.fullmatch(log_fmt, line)
     if m:
         status_code = re.search(status_code_r, line).group(0).strip()
         print(f'{line}, {status_code=}')
