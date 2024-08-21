efc060a45b891309b8d862df67c5a459da208ec7
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-21 02:59:08 +0000

    Start from the biggest coin

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,12 +10,16 @@ def makeChange(coins, total):
     """Find the fewest number of coins needed"""
     if total <= 0:
         return 0
-    num_coins = [total + 1] * (total + 1)
-    num_coins[0] = 0
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                num_coins[i] = min(num_coins[i], 1 + num_coins[i - coin])
-    if num_coins[total] < total + 1:
-        return num_coins[total]
-    return -1
+    coins = sorted(coins, reverse=True)
+    i = 0
+    num_coins = 0
+    n = len(coins)
+    while total > 0:
+        if i >= n:
+            return -1
+        elif total >= coins[i]:
+            total -= coins[i]
+            num_coins += 1
+        else:
+            i += 1
+    return num_coins
